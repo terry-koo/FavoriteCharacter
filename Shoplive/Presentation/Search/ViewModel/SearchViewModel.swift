2d00b45fbf78162b60b510ae8ffb21be7c95bb2b
Terry-Koo <xpflxhfl94@naver.com>
2023-11-16 20:50:09 +0900

    [feat] SearchViewModel 영웅 검색 로직 구현

--- Shoplive/Presentation/Search/ViewModel/SearchViewModel.swift
+++ Shoplive/Presentation/Search/ViewModel/SearchViewModel.swift
@@ -13,6 +13,9 @@ class SearchViewModel {
     var isFetching: Observable<Bool> = Observable(false)
     
     var characterService: CharacterService
+    
+    var searchText: String = ""
+    
     private var pageSize = 10 // 페이지당 아이템 개수
     private var currentPage = 1 // 현재 페이지
     private var totalItems = 0 // 전체 아이템 개수
@@ -49,6 +52,56 @@ class SearchViewModel {
     
     func getCharacterData(index: Int) -> CharacterData {
         return characterCollectionDatas.value[index]
+    }
+    
+    // -- 영웅 검색하기 -- //
+    
+    func searchCharacter(_ name: String) {
+        if !isFetching.value {
+            isFetching.value = true
+            
+            characterService.fetchCharacterCollectionsWithNameStart(name: name, limit: pageSize, offset: (currentPage - 1) * pageSize) { [weak self] result in
+                guard let self = self else { return }
+                
+                switch result {
+                case .success(let response):
+                    self.handleSuccessResponse(response)
+                case .failure(let error):
+                    self.handleErrorResponse(error)
+                }
+                
+                isFetching.value = false
+            }
+        }
+    }
+    
+    func searchTextDidChange(_ searchText: String) {
+        searchDebouncer.call()
+        self.searchText = searchText
+    }
+    
+    private func resetPagination() {
+        currentPage = 1
+        totalItems = 0
+        characterCollectionDatas.value = []
+    }
+    
+    private lazy var searchDebouncer: Debouncer = {
+        return Debouncer(delay: 0.3) { [weak self] in
+            self?.performSearch()
+        }
+    }()
+    
+    private func performSearch() {
+        if searchText.isEmpty {
+            resetPagination()
+            getCharaterCollections()
+        }
+        
+        if searchText.count >= 2 {
+            resetPagination()
+            searchCharacter(searchText)
+        }
     }
     
     // -- 데이터 응답 결과 처리 -- //
