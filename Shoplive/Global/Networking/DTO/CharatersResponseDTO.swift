c6538583b9f60c2d6d5ba656e0102e61c1c9154f
Terry-Koo <xpflxhfl94@naver.com>
2023-11-16 20:39:00 +0900

    [refactor] DTO 프로퍼티 옵셔널로 리팩토링

--- Shoplive/Global/Networking/DTO/CharatersResponseDTO.swift
+++ Shoplive/Global/Networking/DTO/CharatersResponseDTO.swift
@@ -9,26 +9,28 @@ import Foundation
 
 // MARK: - CharatersResponseDTO
 struct CharatersResponseDTO: Codable {
-    let code: Int
-    let data: Response
+    let code: Int?
+    let data: Response?
 }
 
 // MARK: - Response
 struct Response: Codable {
-    let offset, limit, total, count: Int
-    let results: [Character]
+    let offset, limit, total, count: Int?
+    let results: [Character]?
 }
 
 // MARK: - Character
 struct Character: Codable {
-    let id: Int
-    let name, description: String
-    let thumbnail: Thumbnail
+    let id: Int?
+    let name, description: String?
+    let thumbnail: Thumbnail?
+}
+
 }
 
 // MARK: - Thumbnail
 struct Thumbnail: Codable {
-    let path, thumbnailExtension: String
+    let path, thumbnailExtension: String?
 
     enum CodingKeys: String, CodingKey {
         case path
